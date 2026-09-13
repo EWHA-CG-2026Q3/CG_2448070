1f4352e030b135790928d328b3071f4d30942d2b
Yeonwoo Lee <yeonwoo845@gmail.com>
2026-09-13 14:11:10 +0900

    S03:made a Square

--- Assets/S03_Mesh2D/S03_SingleTriangleMesh_Finish.cs
+++ Assets/S03_Mesh2D/S03_SingleTriangleMesh_Finish.cs
@@ -17,7 +17,11 @@ public class S03_SingleTriangleMesh_Finish : MonoBehaviour
             new Vector3(0f, 1f, 1f), // 7
         };
 
-        int[] triangles = new int[3] { 4, 5, 6 };
+        int[] triangles = new int[] {
+        
+            0,2,1,
+            0,3,2
+        };
 
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
